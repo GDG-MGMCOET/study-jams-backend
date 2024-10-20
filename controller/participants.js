a20ff293753e3a5ba4881b5f3523201add3518cf
Jeetu Gupta <jeetu270602@gmail.com>
2024-10-20 16:50:37 +0530

    sort and mask email

--- controller/participants.js
+++ controller/participants.js
@@ -2,7 +2,11 @@ const participantsModel = require("../model/participants");
 
 const getparticipants = async (req, res) => {
   try {
-    const participants = await participantsModel.find();
+    const participants = await participantsModel.find().sort({
+      accessCodeRedeemed: -1,
+      noOfBadges: -1,
+      arcadeGame: -1,
+    });
 
     if (participants.length === 0) {
       return res.send({
@@ -15,12 +19,24 @@ const getparticipants = async (req, res) => {
       });
     }
 
+    const maskedParticipants = participants.map((participant) => {
+      const emailParts = participant.email.split("@");
+      const maskedEmail = `${emailParts[0].substring(0, 2)}***@${
+        emailParts[1]
+      }`;
+
+      return {
+        ...participant._doc,
+        email: maskedEmail,
+      };
+    });
+
     return res.send({
       success: true,
       status: 200,
       message: "Participants retrieved successfully",
       data: {
-        participants,
+        participants: maskedParticipants,
         lastUpdated: participants[0].updatedAt,
       },
     });
